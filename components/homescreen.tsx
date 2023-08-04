6bcd28d5f670af44597536d87be450d4e5efe036
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-04 14:11:56 +0530

    animations

--- components/homescreen.tsx
+++ components/homescreen.tsx
@@ -2,22 +2,44 @@
 import Lottie from 'lottie-react'
 import animationData from '@/public/assets/Animations/animation_lktta0yg.json'
 import Link from 'next/link';
+import {motion as m} from 'framer-motion'
 
 
 const HomeScreen = () => {
     return (
         <section id='home' className=" bg-white min-h-screen flex md:flex-row flex-col-reverse items-center justify-center gap-5 md:gap-10">
-            <div className='md:h-full h-2/5 px-10 md:px-0 md:w-2/5 text-center md:text-right'>
-                <p className='text-lg md:text-xl xl:text-3xl pb-2'>Here at</p>
-                <h1 className='text-5xl lg:text-7xl 2xl:text-8xl font-bold text-textgray'>ScienoMath</h1>
-                <p className='text-lg md:text-xl xl:text-3xl pt-2 text-textblue pb-10 font-semibold'>We learn effectively by adding an element of fun.</p>
+            <m.div
+            className='md:h-full h-2/5 px-10 md:px-0 md:w-2/5 text-center md:text-right'>
+                <m.p
+                initial = {{opacity:0, x:100}}
+                animate = {{opacity:1, x:0}}
+                transition={{delay:0, type:"Tween", duration:1}}
+                className='text-lg md:text-xl xl:text-3xl pb-2'>Here at</m.p>
+                <m.h1
+                initial = {{opacity:0, x:100}}
+                animate = {{opacity:1, x:0}}
+                transition={{delay:0.5, type:"Tween", duration:1}}
+                className='text-5xl lg:text-7xl 2xl:text-8xl font-bold text-textgray'>ScienoMath</m.h1>
+                <m.p
+                initial = {{opacity:0, x:100}}
+                animate = {{opacity:1, x:0}}
+                transition={{delay:1, type:"Tween", duration:1}}
+                className='text-lg md:text-xl xl:text-3xl pt-2 text-textblue pb-10 font-semibold'>We learn effectively by adding an element of fun.</m.p>
                 <Link href="#about">
-                    <button className='bg-buttonorange rounded-xl py-2 px-5'>Learn More {"->"}</button>
+                    <m.button
+                    initial = {{opacity:0, x:100}}
+                    animate = {{opacity:1, x:0}}
+                    transition={{delay:1.5, type:"Tween", duration:1}}
+                    className='bg-buttonorange rounded-xl py-2 px-5'>Learn More {"->"}</m.button>
                 </Link>
-            </div>
-            <div className='md:h-full w-11/12 md:w-2/5'>
+            </m.div>
+            <m.div
+            initial = {{opacity:0, x:-100}}
+            animate = {{opacity:1, x:0}}
+            transition={{delay:0, type:"Tween", duration:1}}
+            className='md:h-full w-11/12 md:w-2/5'>
                 <Lottie loop autoplay animationData={animationData} />
-            </div>
+            </m.div>
         </section>
     );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,9 @@
+'use client'
 import Image from "next/image";
 import logo from "@/public/assets/logo(2).png"
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import {motion as m} from 'framer-motion';
 
 const poppins = Poppins({weight:["400","300"], subsets:["latin"]})
 
@@ -11,25 +13,45 @@ const Navbar = () => {
         <div className="fixed top-0 left-0 z-50 w-full bg-white">
             <div className=" bg-white text-navbartext">
             <div className="flex justify-between items-center py-5 px-10">
-                <div className="my-auto">
+                <m.div
+                initial = {{opacity:0, x:-40}}
+                animate = {{opacity:1, x:0}}
+                transition={{delay:0, duration:1, type: "Tween"}}
+                className="my-auto">
                     <Link href={"#home"}><Image src={logo} height={64} width={150} alt="Restaurant logo" quality={100} /></Link>
-                </div>
+                </m.div>
                 <div className="flex pt-2 text-lg md:gap-5 items-center text-textgray font-semibold">
-                    <button className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
+                    <m.button
+                    initial = {{opacity:0, x:40}}
+                    animate = {{opacity:1, x:0}}
+                    transition={{delay:0, duration:1, type: "Tween"}}
+                    className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
                         <a href="#about">About</a>
-                    </button>
-                    <button className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
+                    </m.button>
+                    <m.button
+                    initial = {{opacity:0, x:40}}
+                    animate = {{opacity:1, x:0}}
+                    transition={{delay:0.5, duration:1, type: "Tween"}}
+                    className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
                         <a href="#features">Features</a>
-                    </button>                    
-                    <button className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
+                    </m.button>                    
+                    <m.button
+                    initial = {{opacity:0, x:40}}
+                    animate = {{opacity:1, x:0}}
+                    transition={{delay:1, duration:1, type: "Tween"}}
+                    className="border-b-4 border-white transition-all ease-in-out  hover:border-buttonorange">
                         <a href="#testimonials">Testimonials</a>
-                    </button>                    
+                    </m.button>                    
                     {/* <button className="border-b-4 border-white transition-all ease-in-out hover:border-buttonorange">
                         <a href="#instructor">Instructor</a>
                     </button>                     */}
-                    <button className="border-b-4 border-white transition-all ease-in-out hover:border-buttonorange">
+                    <m.button
+                    initial = {{opacity:0, x:40}}
+                    animate = {{opacity:1, x:0}}
+                    transition={{delay:1.5, duration:1, type: "Tween"}}
+                    className="border-b-4 border-white transition-all ease-in-out hover:border-buttonorange">
                         <a href="#contact">Contact Us</a>
-                    </button>                
+                    </m.button>                
                 </div>
             </div>
         </div>
