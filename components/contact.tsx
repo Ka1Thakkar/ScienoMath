8b50a3a132bb3b9801f56af413b041cbcb1f45d7
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-04 12:49:14 +0530

    Padding changes

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
     return (
         <section id='contact' className="min-h-screen bg-white flex flex-col justify-center items-center">
             <h1 className='text-5xl md:text-6xl font-semibold pb-10 text-textgray underline underline-offset-8 decoration-4 decoration-textblue'>Contact Us</h1>
-            <p className='text-xl md:text-2xl text-bgbrown font-semibold pb-5 md:pb-20 text-center px-2'>Have any queries? Feel free to contact us.</p>
+            <p className='text-xl md:text-2xl text-bgbrown font-semibold pb-5 md:pb-20 text-center px-5'>Have any queries? Feel free to contact us.</p>
             <div className='flex flex-col md:flex-row justify-center items-center gap-0 lg:gap-20 xl:gap-40 2xl:gap-52 pb-10 md:pb-20'>
                 <div className='flex flex-col justify-center items-center w-8/12 md:w-3/12'>
                     <Image src={logo} height={300} width={300} quality={100} alt='logo' className='pb-5' />
