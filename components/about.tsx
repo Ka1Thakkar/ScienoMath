5c897e2559767f357e508aea11c92c0e036e7d89
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-04 13:47:07 +0530

    About page upper padding removed

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const About = () => {
     return (
-        <section id='about' className=" bg-white min-h-screen flex items-center flex-col md:flex-row-reverse justify-center gap-20 pt-16">
+        <section id='about' className=" bg-white min-h-screen flex items-center flex-col md:flex-row-reverse justify-center gap-20">
             <div className='h-2/5 md:h-full md:w-2/5 text-center'>
                 <h1 className='text-5xl md:text-6xl font-semibold text-textgray underline underline-offset-8 decoration-4 decoration-textblue'>About</h1>
                 <p className='md:text-lg xl:text-2xl pt-5 px-10 md:px-0 pb-5'>Welcome to ScienoMath! We offer interactive online classes that provide visuals to enhance learning retention. With small batch sizes of up to 8 students, we offer personalized attention while covering school syllabus and preparing for competitive exams. Join us for a transformative learning experience that empowers you for a successful future. Let's learn, grow, and succeed together!</p>
